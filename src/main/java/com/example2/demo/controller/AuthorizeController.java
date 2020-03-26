1dded36b5c006bc2c7ccc2901e8aa3c0b8864c2b
zhaoyuloong <zhaoyuloong@qq.com>
2020-03-26 17:26:11 +0800

    sql

--- src/main/java/com/example2/demo/controller/AuthorizeController.java
+++ src/main/java/com/example2/demo/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.example2.demo.controller;
 
 import com.example2.demo.dto.AccessTokenDTO;
 import com.example2.demo.dto.GithubUser;
+import com.example2.demo.mapper.UserMapper;
+import com.example2.demo.model.User;
 import com.example2.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +29,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request
                            ) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -35,12 +44,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accession = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user=githubProvider.getUser(accession);
-        if (user.getName()==null){
-            System.out.printf("name为空");
+        GithubUser githubUser=githubProvider.getUser(accession);
+        if (githubUser==null){
+            // login in fail
+            return "redirect:/";
         }else {
-            System.out.printf(user.getName());
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
         }
-        return "index";
     }
 }
